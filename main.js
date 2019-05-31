8afdf7547530f19749a091a60e2d88c39396b948
dev-nas <maan7224@gmail.com>
2019-05-31 15:14:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+i versionne avec git
+git init
+git add nom du fichier (main.js ou index.html)
+git commit message'_first-commit'
+git status pour vérifier le status de tes dossier
+ls-a pour enlever le master 
+-rf
+
+
+on ne quitte pas son poste si on a pas 
+
+atus
+On branch master
+nothing to comit,working tree
+clean
+first-js-project(git)écrit en violet
+first-js-project(git)écrit en violet master
+
